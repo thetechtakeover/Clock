adabe95deb8599fe8525f5c43138c521201bbbe2
Evan Poole <thetechtakeover@gmail.com>
2020-10-11 23:50:06 -0400

    correction ;line 12 and setClock

--- js.js
+++ js.js
@@ -8,13 +8,14 @@ function setClock() {
     const currentDate= new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minutesRatio + .getHours()) / 12
-    setRotation(secondHand, sectionsRatio)
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(secondHand, hoursRatio)
-
 }
 
 function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
+
+setClock()
